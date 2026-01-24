315791342924b142bbd0f370e21244f2aab25e70
Epebifie Treasure <treepebifie@gmail.com>
2026-01-24 02:40:03 +0100

    new_changes

--- src/Routes/wallet.routes.js
+++ src/Routes/wallet.routes.js
@@ -1,10 +1,12 @@
 const express = require ('express')
 const { createRedirectUrl } = require('../Controller/flutterwave.controller')
-const { isAuth } = require ('../Config/auth.js')
+const isAuth = require('../Config/auth')
+
 
 const Route = express.Router()
 
 Route.post ('/make-payment', isAuth, createRedirectUrl)
+
 
 
 module.exports = Route
